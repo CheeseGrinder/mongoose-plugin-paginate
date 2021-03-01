9eb0a4f29a552c98b91a95fbdc09a7a97175581b
Sukaato <sukaato.dev@gmail.com>
2021-03-01 23:41:50 +0100

    🐛 Fix types declarations

--- src/module.ts
+++ src/module.ts
@@ -1,26 +1,28 @@
+import 'mongoose';
+
 declare module 'mongoose' {
-  interface PaginateOption<T extends Document> {
+
+  export interface PaginateOption<T extends Document> {
     query?: FilterQuery<T>;
     page?: number;
     limit?: number;
     populate?: PopulateOptions | PopulateOptions[];
   }
 
-  interface PaginateResult<T extends Document> {
+  export interface PaginateResult<T extends Document> {
     docs: T[];
     docsCount: number;
     page: number,
-    logicalPage: number,
     limit: number;
     pagesCount: number;
     hasPrev: boolean;
     hasNext: boolean;
   }
 
-  interface PaginateModel<T extends Document> extends Model<T> {
+  export interface PaginateModel<T extends Document> extends Model<T> {
     paginate(): Promise<PaginateResult<T>>;
     paginate(options: PaginateOption<T>): Promise<PaginateResult<T>>;
   }
 
-  function model<T extends Document>(name: string, schema?: Schema<T>, collection?: string, skipInit?: boolean): PaginateModel<T>;
+  export function model<T extends Document>(name: string, schema?: Schema<T>, collection?: string, skipInit?: boolean): PaginateModel<T>;
 }
